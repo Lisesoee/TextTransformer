6bb7f239ed84c37b3410576c93a0ff838d54a2b2
Lise Jacobsen <lise.jacobsen@hansencx.com>
2024-01-07 15:35:45 +0100

    Add project files.

---
+++ TextTransformer/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TextTransformation.Services;
+
+namespace TextTransformation.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string TransformedResult { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnPostTransform(string text)
+        {
+            var myTextTransformer = new TextTransformerService();
+            TransformedResult = await myTextTransformer.TransformText(text);
+            //TransformedResult = "Wello Horld (simulated)";
+
+            return Page();
+        }
+
+    }
+}
---
+++ TextTransformer/Services/TextTransformerService.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+
+namespace TextTransformation.Services
+{
+    public class TextTransformerService
+    {
+        private static RestClient spoonerismRestClient = new RestClient("http://spoonerism/Spoonerism/");
+
+        public async Task<string> TransformText(string text)
+        {
+            var task = spoonerismRestClient.GetAsync<string>(new RestRequest("/GetSpoonerizedResult?text=" + text));
+            await task;
+            if (task?.Status == TaskStatus.RanToCompletion)
+            {
+                Console.WriteLine("Retrived result from spoonerism service: " + task.Result);
+                return task.Result;
+            }
+            if (task?.Status == TaskStatus.Faulted)
+            {
+                throw new Exception("Spoonerism request failed. Task status: " + task?.Status);
+            }
+            throw new Exception("Unexpected Task status: " + task?.Status);
+        }
+    }
+}
